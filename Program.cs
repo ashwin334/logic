f0e55a4d0c6531b2051466691e0d47f737db5e39
ashwin333 <ashwinpatil832@gmail.com>
2022-10-09 15:09:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            int b = a++;  // b=10  a=11
+
+            int c = ++b + ++a; // 11 + 12    
+
+            Console.WriteLine(a); //
+            Console.WriteLine(b); //
+            Console.WriteLine(c); //
+
+
+        }
+    }
+}
